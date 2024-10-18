c884be462583cbd0a7668d16a70467cb38eba624
shadowhexer <austinmanlangit@gmail.com>
2024-10-18 15:40:20 +0800

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <header>
+    <nav>
+      <Header />
+    </nav>
+    
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Notifications</li>
+            <li>Messages</li>
+            <li>Bookmarks</li>
+            <li>Store</li>
+            <li>Cart</li>
+            <li>Payment Methods</li>
+            <li>My Profile</li>
+            <li>Settings</li>
+        </ul>
+    </nav>
+</template>
